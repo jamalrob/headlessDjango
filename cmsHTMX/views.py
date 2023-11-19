155185c47271c3db4414248168454bb9f5fedbe4
Alistair Robinson <alistair@commissioncrowd.com>
2023-11-19 09:14:20 +0300

    Turned on gpt

--- cmsHTMX/views.py
+++ cmsHTMX/views.py
@@ -87,28 +87,27 @@ def get_suggestion(request):
                 given content written by the user (writer)."""
 
     # TESTING
-    content = "There once was a green large lake at the bottom of my garden but now it's gone and I be unsure exakerly why."
+    # content = "There once was a green large lake at the bottom of my garden but now it's gone and I be unsure exakerly why."
 
     # REAL
-    #content = f'Here is an excerpt from my latest article: { content_to_fix }'
+    content = f'Here is an excerpt from my latest article: { content_to_fix }'
 
     # TESTING
-    suggestion = "Lorem Ipsum is simply dummy\n\n text of the printing\n\n and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,\n\n when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    #suggestion = "Lorem Ipsum is simply dummy\n\n text of the printing\n\n and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,\n\n when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
     # REAL
-    #completion = openai.ChatCompletion.create(
-    #    model="gpt-3.5-turbo",
-    #    #model="gpt-4", # No access till I pay
-    #    messages=[
-    #        {"role": "system", "content": setup},
-    #        {"role": "user", "content": content}
-    #    ]
-    #)
-    #suggestion = completion.choices[0].message.content
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": setup},
+            {"role": "user", "content": content}
+        ]
+    )
+    suggestion = completion.choices[0].message.content
 
     # TESTING
-    import time
-    time.sleep(2)
+    #import time
+    #time.sleep(2)
 
     context = {
         'original': content_to_fix,
